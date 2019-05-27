2ede490da36f08cac76a69947906f063a9143dfc
Ming Li <ming.george.li@gmail.com>
2019-05-27 16:29:17 +0100

    initialise supervisor

--- runtime-dev/start.sh
+++ runtime-dev/start.sh
@@ -7,3 +7,7 @@ sudo service ssh restart
 ROOT_PASSWORD=`pwgen -c -n -1 12`
 echo "root:$ROOT_PASSWORD" | chpasswd
 echo "root login password: $ROOT_PASSWORD"
+
+# Launch supervisor
+supervisord -n
+
