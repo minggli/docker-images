13460f90d03220a2d17c58252d44267e93b947fe
Ming Li <ming.george.li@gmail.com>
2020-01-09 09:42:44 +0000

    add aws credentials

--- edf_env_up.sh
+++ edf_env_up.sh
@@ -4,5 +4,6 @@ docker run -td -p 2020:22 \
 	--restart=always \
 	-v $HOME/.ssh/:/root/.ssh/ \
 	-v $HOME/repos/:/root/repos/ \
+	-v $HOME/.aws/:/root/.aws/ \
 	-v $HOME/aws-mfa-script-master/:/root/aws-mfa-script-master/ \
 	-t $container_tag
