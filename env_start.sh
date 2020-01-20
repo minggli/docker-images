f1680f4c888d0044f36b68ec291bda3e5680a0bb
Ming Li <ming.george.li@gmail.com>
2020-01-20 12:28:28 +0000

    expose 8888 for jupyter

--- env_start.sh
+++ env_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 container_passd=$(docker logs $(docker ps -f volume=$HOME/repos -q) | grep 'login password:' | tail -1 | sed 's/^.*: //')
-sshpass -p $container_passd ssh -o StrictHostKeyChecking=no -p 2020 -L 5000:localhost:5000 -R 52698:localhost:52698 root@localhost
+sshpass -p $container_passd ssh -o StrictHostKeyChecking=no -p 2020 -L 5000:localhost:5000 -L 8888:localhost:8888 -R 52698:localhost:52698 root@localhost
