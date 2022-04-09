fa99e7a63b00246d7b94eb67bc8e81119e49b449
Ming Li <ming.george.li@gmail.com>
2022-04-09 21:00:54 +0200

    reformatting

--- env_start.sh
+++ env_start.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 ssh -o StrictHostKeyChecking=no -p 2020 -L 5000:localhost:5000 -L 8888:localhost:8888 -R 52698:localhost:52698 root@localhost
--- env_up.sh
+++ env_up.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
 container_tag=$1
 docker run \
   -td \
   -p 2020:22 \
-  --cpus=2 \
-  --memory=2g \
-  --memory-swap=2g \
-  --restart=always \
+  --cpus 2 \
+  --memory 2g \
+  --memory-swap 2g \
+  --restart unless-stopped \
   -v $HOME/repos/:/root/repos/ \
   -t $container_tag
