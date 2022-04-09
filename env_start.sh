4703117ac00dc218f13f1715e665fb16ed7468d9
Ming Li <ming.george.li@gmail.com>
2022-04-09 21:25:03 +0200

    reformat

--- env_start.sh
+++ env_start.sh
@@ -1,1 +1,6 @@
-ssh -o StrictHostKeyChecking=no -p 2020 -L 5000:localhost:5000 -L 8888:localhost:8888 -R 52698:localhost:52698 root@localhost
+ssh -o StrictHostKeyChecking=no \
+    -p 2020 \
+    -L 5000:localhost:5000 \
+    -L 8888:localhost:8888 \
+    -R 52698:localhost:52698 \
+    root@localhost
