1de27f98cc4bf3dd1c6e8cfbc76eb0b4202690a3
Ming Li <ming.george.li@gmail.com>
2022-04-10 02:23:05 +0200

    forward ssh socket

--- env_up.sh
+++ env_up.sh
@@ -1,4 +1,11 @@
 container_tag=$1
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  os_ssh_auth_sock=/run/host-services/ssh-auth.sock
+else
+  os_ssh_auth_sock=${SSH_AUTH_SOCK}
+fi
+
 docker run \
   -td \
   -p 2020:22 \
@@ -6,5 +13,7 @@ docker run \
   --memory 2g \
   --memory-swap 2g \
   --restart unless-stopped \
-  -v $HOME/repos/:/root/repos/ \
+  --volume $HOME/repos/:/root/repos/ \
+  --mount type=bind,source=${os_ssh_auth_sock},target=/tmp/ssh_agent.sock \
+  --env SSH_AUTH_SOCK=/tmp/ssh_agent.sock \
   -t $container_tag
