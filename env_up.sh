5ed0f4474f01d5fc333196758ce3f9b6a09e477d
Ming Li <ming.george.li@gmail.com>
2022-06-17 20:06:04 +0200

    remove swap memory

--- env_up.sh
+++ env_up.sh
@@ -9,7 +9,6 @@ fi
 docker run \
   -td \
   -p 2020:22 \
-  --memory-swap 2g \
   --restart unless-stopped \
   --volume $HOME/repos/:/root/repos/ \
   --mount type=bind,source=${os_ssh_auth_sock},target=/tmp/ssh_agent.sock \
